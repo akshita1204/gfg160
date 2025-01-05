a26c90065027d9148dd63092319aa79c43183f57
akshita1204 <akshitatyagi04@gmail.com>
2025-01-05 07:17:39 +0530

    commit changes

---
+++ Two Pointers/Day52.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    int countPairs(vector<int> &arr, int target) {
+        // Your code here
+        int c=0;
+        sort(arr.begin(),arr.end());
+        // 1 2 2 3 4 5 
+        int i=0,j=arr.size()-1;
+        while(i<j)
+        {
+            if(arr[i]+arr[j]<target) 
+            {
+                c+=(j-i);
+                i++;
+            }
+            else if(arr[i]+arr[j]>=target) j--;
+        }
+        return c;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int target;
+        cin >> target;
+        cin.ignore();
+        Solution ob;
+        int res = ob.countPairs(arr, target);
+        cout << res << endl;
+        cout << "~\n";
+    }
+    return 0;
+}
+/*
+https://www.geeksforgeeks.org/batch/gfg-160-problems/track/two-pointer-technique-gfg-160/problem/count-pairs-whose-sum-is-less-than-target
+*/
