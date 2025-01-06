f3c1486f584d82de015e3f31e739906f3d26dea9
akshita1204 <akshitatyagi04@gmail.com>
2025-01-06 06:15:31 +0530

    commit changes

---
+++ Two Pointers/Day53.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    vector<int> sumClosest(vector<int>& arr, int target) {
+        // code here
+        int curdif=INT_MAX; 
+        vector<int>ans;
+        sort(arr.begin(),arr.end());
+        int i=0,j=arr.size()-1;
+        while(i<j)
+        {
+            int sum=arr[i]+arr[j];
+            if(abs(target-sum)<curdif)
+            {
+                curdif=abs(target-sum);
+                ans.clear();
+                ans.push_back(arr[i]);
+                 ans.push_back(arr[j]);
+            }
+            if(sum<target) i++;
+            else j--;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int target = stoi(ks);
+        Solution ob;
+        vector<int> ans = ob.sumClosest(arr, target);
+        if (ans.empty()) {
+            cout << "[]";
+        } else {
+            for (auto it : ans) {
+                cout << it << " ";
+            }
+        }
+        cout << endl;
+        cout << "~\n";
+    }
+
+    return 0;
+}
+/*
+https://www.geeksforgeeks.org/batch/gfg-160-problems/track/two-pointer-technique-gfg-160/problem/pair-in-array-whose-sum-is-closest-to-x1124
+*/
