70e77e8bbd2cbd91a3345bf50d9b36f5392cea3b
akshita1204 <akshitatyagi04@gmail.com>
2025-03-02 05:54:23 +0530

    commit changes

---
+++ Queue/Day108.cpp
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function template for C++
+
+class Solution {
+  public:
+    // Function to find maximum of each subarray of size k.
+    vector<int> maxOfSubarrays(vector<int>& arr, int k) {
+        int n=arr.size();
+        vector<int>res;
+        deque<int>dq;
+        for(int i=0;i<n;i++)
+        {
+         //maintain the size of window
+         while(!dq.empty() and dq.front()<=i-k) dq.pop_front();
+         while(!dq.empty() and arr[i]>=arr[dq.back()]) dq.pop_back();
+         dq.push_back(i);
+         if(i>=k-1) res.push_back(arr[dq.front()]);
+        }
+        return res;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // Ignore newline character after t
+
+    while (t--) {
+        vector<int> arr;
+        int k;
+        string inputLine;
+
+        getline(cin, inputLine); // Read the array input as a line
+        stringstream ss(inputLine);
+        int value;
+        while (ss >> value) {
+            arr.push_back(value);
+        }
+
+        cin >> k;
+        cin.ignore(); // Ignore newline character after k input
+
+        Solution obj;
+        vector<int> res = obj.maxOfSubarrays(arr, k);
+        for (int i = 0; i < res.size(); i++)
+            cout << res[i] << " ";
+        cout << endl;
+        cout << "~"
+             << "\n";
+    }
+
+    return 0;
+}
+
+/*https://www.geeksforgeeks.org/batch/gfg-160-problems/track/queue-and-deque-gfg-160/problem/maximum-of-all-subarrays-of-size-k3101*/ 
