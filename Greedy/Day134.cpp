13467d656c479dfba4264497ab8a01d7bcd7b847
akshita1204 <akshitatyagi04@gmail.com>
2025-03-30 06:13:13 +0530

    day 134

---
+++ Greedy/Day134.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+// C++ program to find union of
+// two sorted arrays
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+/*You are required to complete this method*/
+class Solution {
+  public:
+    int startStation(vector<int> &gas, vector<int> &cost) {
+        // Your code here
+         int totalGas = 0, totalCost = 0;
+        int tank = 0, startIndex = 0;
+
+        for (int i = 0; i < gas.size(); i++) {
+            totalGas += gas[i];
+            totalCost += cost[i];
+            tank += gas[i] - cost[i];
+
+            if (tank < 0) {
+                startIndex = i + 1;
+                tank = 0;
+            }
+        }
+
+        return (totalGas >= totalCost) ? startIndex : -1;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+/* Driver program to test above function */
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+
+    while (t--) {
+
+        vector<int> gas, cost;
+        string input1;
+        getline(cin, input1);
+        stringstream ss(input1);
+        int number1;
+        while (ss >> number1) {
+            gas.push_back(number1);
+        }
+        string input2;
+        getline(cin, input2);
+        stringstream sss(input2);
+        int number2;
+        while (sss >> number2) {
+            cost.push_back(number2);
+        }
+        Solution ob;
+        int ans = ob.startStation(gas, cost);
+
+        cout << ans << endl;
+        cout << "~\n";
+    }
+
+    return 0;
+}
+
+//https://www.geeksforgeeks.org/problems/circular-tour-1587115620/1
